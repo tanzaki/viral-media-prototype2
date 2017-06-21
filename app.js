4ff7a441bb28574f14228f843c6be21f5fe40f83
haonx <qtvhao@gmail.com>
2017-06-22 02:00:11 +0700

    jQuery create buttons for switch forms

--- app.js
+++ app.js
@@ -2,5 +2,12 @@
  * Created by qtvha on 6/22/2017.
  */
 jQuery(function ($) {
+    //BEGIN: Setup events for Buttons switch form
+    var btn_choose_files = $('<div class="col-xs-3 button-switch-form">Choose files</div>');
+    var btn_paste_image = $('<div class="col-xs-3 button-switch-form">Paste image url</div>');
+    var btn_paste_video = $('<div class="col-xs-3 button-switch-form">Paste video url</div>');
+    var btn_make_meme = $('<div class="col-xs-3 button-switch-form">Make meme</div>');
+    //END: Setup events for Buttons switch form
+
     $('#myModal').modal();
 });
