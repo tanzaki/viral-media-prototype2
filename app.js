f2717784ac317809857dc194b9d1c2a7a00605f5
haonx <qtvhao@gmail.com>
2017-06-22 07:42:25 +0700

    separate texts to div blocks - tách text vào các khối div

--- app.js
+++ app.js
@@ -3,11 +3,23 @@
  */
 jQuery(function ($) {
     //BEGIN: Setup events for Buttons switch form
-    var btn_choose_files = $('<div class="col-xs-3 button-switch-form">Choose files</div>');
-    var btn_paste_image = $('<div class="col-xs-3 button-switch-form">Paste image url</div>');
-    var btn_paste_video = $('<div class="col-xs-3 button-switch-form">Paste video url</div>');
-    var btn_make_meme = $('<div class="col-xs-3 button-switch-form">Make meme</div>');
+    var btn_choose_files = $('<div class="button-switch-form"></div>');
+    var btn_paste_image = $('<div class="button-switch-form"></div>');
+    var btn_paste_video = $('<div class="button-switch-form"></div>');
+    var btn_make_meme = $('<div class="button-switch-form"></div>');
     //END: Setup events for Buttons switch form
+    $('#form-upload').find('.buttons-switch-form').append(btn_choose_files,btn_paste_image,btn_paste_video,btn_make_meme);
+
+    btn_choose_files.wrap($('<div class="col-xs-3"></div>'));
+    btn_paste_image.wrap($('<div class="col-xs-3"></div>'));
+    btn_paste_video.wrap($('<div class="col-xs-3"></div>'));
+    btn_make_meme.wrap($('<div class="col-xs-3"></div>'));
+
+
+    btn_choose_files.addClass('icon-box-choose_files').prepend('<div class="icon_box-text">Choose files</div>').prepend('<div class="icon"></div>');
+    btn_paste_image.addClass('icon-box-paste_image').prepend('<div class="icon_box-text">Paste image url</div>').prepend('<div class="icon"></div>');
+    btn_paste_video.addClass('icon-box-paste_video').prepend('<div class="icon_box-text">Paste video url</div>').prepend('<div class="icon"></div>');
+    btn_make_meme.addClass('icon-box-make_meme').prepend('<div class="icon_box-text">Make meme</div>').prepend('<div class="icon"></div>');
 
     $('#myModal').modal();
 });
